d9794f875333e3976d266bfd67772bb3bde58b5d
flynneva <evanflynn.msu@gmail.com>
2019-12-27 17:15:58 -0800

    search columns too

--- static/script/tableHelp.js
+++ static/script/tableHelp.js
@@ -1,21 +1,31 @@
 function myFunction() {
   // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
+  var input, filter, table, tr, tc, td, i, j, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("todays_games");
   tr = table.getElementsByTagName("tr");
+  tc = table.getElementsByTagName("tc");
 
   // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
+  for (i = 0; i < tr.length; i++) { 
+    for (j = 0; j < tc.length; j++) {
+      td = tr[i].getElementsByTagName("td")[j];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
       }
+    }
+
+    // Searched row, if found match to filter then hide row
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
     }
   }
 }
