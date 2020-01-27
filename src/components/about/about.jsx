c7e2be9cbe4d28bd21b85646d3df883bc9cb49a4
flynneva <evanflynn.msu@gmail.com>
2020-01-26 19:34:06 -0800

    fixed typo in about page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -20,7 +20,7 @@ function About () {
       <p>Hey! Thanks for taking the time out of your day to read a little bit about who I am. You must be really scraping the bottom of the barrel of the internet if you are reading this right now...</p>
       <p>In short, things I like are pretty much all shown here on this website. Sports, Robotics and Finance. Things not listed on this website would be things like government, the environment, and being outside.</p>
      
-      <p>A perfect Sunday for me starts off with a morning hike or run, something delicious for breakfast with a hot cup of coffee, followed up with a few hours spent at a local coffee spot working on some software or a personal projec around the house.</p>
+      <p>A perfect Sunday for me starts off with a morning hike or run, something delicious for breakfast with a hot cup of coffee, followed up with a few hours spent at a local coffee spot working on some software or a personal project around the house.</p>
 
       <p>Depending on what you're interested in, feel free to check out more of this website or follow the links below. Thanks again for even reading this!</p>
       
