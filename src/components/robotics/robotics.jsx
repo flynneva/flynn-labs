26d36acfaa5e516f37c0006075ab521a9e542519
flynneva <evanflynn.msu@gmail.com>
2020-03-27 20:19:07 -0700

    made 3d world full screen

--- src/components/robotics/robotics.jsx
+++ src/components/robotics/robotics.jsx
@@ -14,28 +14,20 @@ function Robotics () {
       minHeight: '100vh',  
     }
    
+    const itemStyle = {
+      width: '100vw',
+      height: '100%',
+      minHeight: '100vh',
+      padding: 0,
+      margin: 0
+    }
+ 
     return (
-    <Grid container justify="center" spacing={1} style={gridStyle}>
-        <Grid item xs={11}>
-            <Card>
-                <CardContent>
-                    <Typography variant='h4' style={{ textAlign: 'center' }}>Robotics</Typography>
-                    <br></br>
-                    <Grid container spacing={3}>
-                      <Grid item xs={12}>
-                        <Typography variant='body2' style={{ textAlign: 'center' }}>
-                          Here is a quick demo until I get some time to add actual functionality to this page. Hope you like it!
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12} style={{ height: '65vh', marginBottom: 8 }}>
-                        <World>
-                         
-                        </World>
-                      </Grid>
-                    </Grid>
-                </CardContent>
-            </Card>
-        </Grid>
+    <Grid container justify="center" spacing={0} style={gridStyle}>
+      <Grid item xs={12} style={itemStyle}>
+        <World>
+        </World>
+      </Grid>
     </Grid>
     );
 }
--- src/components/three/plane/Plane.js
+++ src/components/three/plane/Plane.js
@@ -5,8 +5,8 @@ function Plane(props) {
   const [ref] = usePlane(() => ({ mass: 0, ...props }))
   return (
     <mesh ref={ref} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[5, 5]} />
-      <meshPhongMaterial attach="material" color="#171717" />
+      <planeBufferGeometry attach="geometry" args={[50, 50]} />
+      <meshPhongMaterial attach="material" color={props.color} />
     </mesh>
   )
 }
